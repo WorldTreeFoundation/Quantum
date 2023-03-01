ddbbaae1a73b8296d824ddbf062eaac1ec6d6210
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2023-03-01 19:29:57 +0100

    Point class implemented

--- quantum/include/point/Point.h
+++ quantum/include/point/Point.h
@@ -1,9 +1,20 @@
 #pragma once
+#include <array>
 
-// n-dimensional position of a Quant in a World
+// n-dimensional position of the Quant in the World
+template <typename T, unsigned int n>
 class Point
 {
 public:
-    Point() {}
+    Point(std::array<T, n> coords) : coords(coords) {}
     ~Point() {}
+
+    T operator[](const size_t &i) const
+    {
+        return coords[i];
+    }
+
+public:
+    // Point's coordinates
+    mutable std::array<T, n> coords;
 };
--- quantum/include/wave_function/wave_function.h
+++ quantum/include/wave_function/wave_function.h
@@ -3,12 +3,13 @@
 
 // A mathematical description of the quantum state 
 // of an isolated Quant
+template <typename T, unsigned int n>
 class WaveFunction
 {
 public:
     // Measure wave function at given Point
-    virtual float measure_at(Point) = 0;
+    virtual float measure_at(Point<T, n>&) = 0;
     // Calculate the complementary events sum for further 
-    // validation (must equal to 1)
-    virtual bool complementary_events_sum() = 0;
+    // validation (must equal to 1.0)
+    virtual float complementary_events_sum() = 0;
 };
