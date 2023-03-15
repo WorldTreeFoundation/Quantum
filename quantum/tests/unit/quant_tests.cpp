aec64fe8973640b2589e0550003b292e9ceea271
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2023-03-15 21:20:04 +0100

    tests fixes

--- quantum/tests/unit/quant_tests.cpp
+++ quantum/tests/unit/quant_tests.cpp
@@ -2,10 +2,11 @@
 #include "quantum/Quantum.h"
 #include <iostream>
 
-class TestWF: public WaveFunction<int, 1>
+class TestWF : public WaveFunction<int, 1>
 {
 public:
-    float measure_at(Point<int, 1> &p) const override {
+    float measure_at(Point<int, 1> &p) const override
+    {
         return (p[0] == 1) ? 1 : 0;
     }
     float complementary_events_sum() const override { return 1; }
@@ -14,18 +15,15 @@ public:
 class QuantumTests : public ::testing::Test
 {
 protected:
-    TestWF * wf;
-    Quant<int, 1> * q;
+    Quant<int, 1> *q;
 
     virtual void SetUp()
-    {      
-        wf = new TestWF;
-        q = new Quant<int, 1>(wf);
+    {
+        q = new Quant<int, 1>(new TestWF);
     }
     virtual void TearDown()
     {
-       delete wf;
-       delete q;
+        delete q;
     }
 };
 
@@ -33,7 +31,8 @@ TEST_F(QuantumTests, measure_at_test)
 {
     Point<int, 1> p({0});
 
-    for (int i = -1; i < 3; i++) {
+    for (int i = -1; i < 3; i++)
+    {
         p = Point<int, 1>({i});
         int expected = (i == 1) ? 1 : 0;
         EXPECT_EQ(q->measure_at(p), expected);
--- quantum/tests/unit/wave_function_tests.cpp
+++ quantum/tests/unit/wave_function_tests.cpp
@@ -2,10 +2,11 @@
 #include "quantum/Quantum.h"
 #include <iostream>
 
-class TestWF: public WaveFunction<int, 1>
+class TestWF : public WaveFunction<int, 1>
 {
 public:
-    float measure_at(Point<int, 1> &p) const override {
+    float measure_at(Point<int, 1> &p) const override
+    {
         return (p[0] == 1) ? 1 : 0;
     }
     float complementary_events_sum() const override { return 1; }
@@ -14,15 +15,15 @@ public:
 class WaveFunctonTests : public ::testing::Test
 {
 protected:
-    TestWF * wf;
+    TestWF *wf;
 
     virtual void SetUp()
-    {      
+    {
         wf = new TestWF;
     }
     virtual void TearDown()
     {
-       delete wf;
+        delete wf;
     }
 };
 
@@ -30,7 +31,8 @@ TEST_F(WaveFunctonTests, measure_at_test)
 {
     Point<int, 1> p({0});
 
-    for (int i = -1; i < 3; i++) {
+    for (int i = -1; i < 3; i++)
+    {
         p = Point<int, 1>({i});
         int expected = (i == 1) ? 1 : 0;
         EXPECT_EQ(wf->measure_at(p), expected);
